4e3a7bca3852872461608d8eef05e818c746f607
Yao Zi <me@ziyao.cc>
2026-04-01 15:24:28 +0000

    clk: sunxi-ng: Add PRCM, RTC, main CCU drivers for A733
    Signed-off-by: Yao Zi <me@ziyao.cc>

---
+++ drivers/clk/sunxi-ng/ccu-sun60i-a733-r.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright(c) 2020 - 2023 Allwinner Technology Co.,Ltd. All rights reserved. */
+/*
+ * Copyright (c) 2023 rengaomin@allwinnertech.com
+ * Copyright (c) 2026 Yao Zi <me@ziyao.cc>
+ */
+
+#ifndef _CCU_SUN60IW2_R_H
+#define _CCU_SUN60IW2_R_H
+
+#include <dt-bindings/clock/sun60i-a733-r-ccu.h>
+#include <dt-bindings/reset/sun60i-a733-r-ccu.h>
+
+#endif /* _CCU_SUN60IW2_R_H */
